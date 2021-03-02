ed0cdf20bf3f2a03aec9cd7886f04277273ec1b6
zyy <1127263614@qq.com>
2021-03-02 21:58:09 +0800

    add 进度条

---
+++ src/views/Test/components/Progress.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="progress">
+    <van-progress :percentage="percent" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Progress',
+  props: {
+    percent: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
